d1ace722b8385326cae2e9191b9cfd14b2948492
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2024-05-13 13:14:46 +0200

    setup nuxt project, added pinia and nuxt ui as dependencies

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useBoardStore } from '../stores/boardStore';
+
+const boardStore = useBoardStore();
+</script>
+
+<template>
+  <main>
+    <div>Build a trello clone home page</div>
+    <pre>{{ boardStore.board }}</pre>
+  </main>
+</template>
